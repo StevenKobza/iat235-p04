c6de769394f06e5ae0eebf9c70632c4f2d3a6540
Steven Kobza <steven.kobza@live.com>
2020-07-31 18:04:15 -0700

    working on mobile dropdown

--- js/dropDownMenu.js
+++ js/dropDownMenu.js
@@ -1,12 +1,15 @@
 
 var el = document.querySelector(".dropDownMenu");
+var mobileEl = document.querySelector(".mobileDropDownMenu");
 
 window.onload = function() {
     console.log("this");
     el.style.display = "none";
+    mobileEl.display = "none";
 }
 
 var btn = document.querySelector(".dropDown");
+var mobileBtn = document.querySelector(".hamburgerClass");
 console.log(btn.textContent);
 
 btn.onclick = function() {
@@ -16,3 +19,19 @@ btn.onclick = function() {
         el.style.display = "none";
     }
 }
+
+mobileBtn.onclick = function() {
+    console.log("this");
+    console.log(mobileEl.style.display);
+    
+    var tempEl = document.getElementById("mobileNavGrid");
+    if (mobileEl.style.display == "none") {
+        mobileEl.style.display = "unset";
+        tempEl.style.gridTemplateRows = "repeat(8, 1fr)";
+    } else {
+        tempEl.style.gridTemplateRows = "repeat(3, 1fr)";
+        mobileEl.style.display = "none";
+    }
+}
+
+
